27d5d9c63843b3a00f944e9d2f2861e893d57db3
Ramsey Dow <yesmar@speakeasy.net>
2008-02-23 22:15:59 -0800

    release 0.0.3

--- lib/iana.rb
+++ lib/iana.rb
@@ -3,9 +3,12 @@
 # iana.rb
 # yesmar@speakeasy.net
 
+$KCODE = 'UTF8'
+require 'jcode'
+
 module IANA
   IANA::NAME = 'IANA'
-  IANA::VERSION = '0.0.2'
+  IANA::VERSION = '0.0.3'
   IANA::COPYRIGHT = 'Copyright (c) 2008 Ramsey Dow'
   def self.copyright() IANA::COPYRIGHT end
   def self.version() IANA::VERSION end
@@ -13,6 +16,7 @@ module IANA
 end
 
 $LOAD_PATH.unshift(File.dirname(__FILE__)+'/iana')
+puts File.dirname(__FILE__)+'/iana'
 
 require 'port'
 require 'protocol'
