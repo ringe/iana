be7dab24a4aa0930a69d947d0120ee04cd76f9a7
Ramsey Dow <yesmar@speakeasy.net>
2009-01-21 14:05:48 -0800

    updating for Ruby 1.9.1 RC1

--- lib/iana.rb
+++ lib/iana.rb
@@ -3,13 +3,10 @@
 # iana.rb
 # yesmar@speakeasy.net
 
-$KCODE = 'UTF8'
-require 'jcode'
-
 module IANA
   IANA::NAME = 'iana'
-  IANA::VERSION = '0.0.4'
-  IANA::COPYRIGHT = 'Copyright (c) 2008 Ramsey Dow'
+  IANA::VERSION = '0.0.5'
+  IANA::COPYRIGHT = 'Copyright (c) 2008-2009 Ramsey Dow'
   def self.copyright() IANA::COPYRIGHT end
   def self.version() IANA::VERSION end
   def self.libdir() File.expand_path(__FILE__).gsub(%r/\.rb$/, '') end
